4cde778769259b70740e5ecb9d94c668731eda59
meherkothari8129 <87307807+meherkothari8129@users.noreply.github.com>
2022-01-13 16:59:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+video = "";
+object_name = "";
+
+function preload()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
